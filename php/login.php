7dc36d916657be0b4c08d5b5a8d4111aa265c65d
UmmeSalmaShabbir Gadriwala <gadriwau@mcmaster.ca>
2018-11-23 14:04:24 -0500

    fixing login :'(

--- php/login.php
+++ php/login.php
@@ -1,21 +1,24 @@
 <?php
-require_once("dbConnect.php"); // connect to database
+require("dbConnect.php"); // connect to database
 
-// validate user sign in form input
+// if user has pressed the submit button
 if (isset($_POST['signin'])) {
-    if (isset($_POST['username']) && $_POST['username'] != '' &&
-        isset($_POST['password']) && $_POST['password'] != '') {
+    // if the username and password fields are not empty or null
+    if (isset($_POST['username']) && $_POST['username'] !== '' &&
+        isset($_POST['password']) && $_POST['password'] !== '') {
+            // if password input and database password match
             if (checkPassword($_POST['username'], $_POST['password'])) {
                 session_start();
                 $_SESSION['isLoggedIn'] = true;
                 $_SESSION['username'] = $_POST['username'];
-                header("Location: https://" + $_SERVER['HTTP_HOST'] + "signedin.php");
+                echo "Login successful.";
+                // header("Location: http://" + $_SERVER['HTTP_HOST'] + "signedin.php");
                 exit;
             } else {
                 die("Incorrect username/password combination");
             }
         } else {
-            echo 'alert("Enter username and password to sign in.")';
+            die("Enter username/password to sign in.");
         }
 }
 
@@ -25,10 +28,10 @@ function checkPassword($username, $password) {
         $query = $conn->prepare(
             'SELECT * FROM user
             WHERE
-            `username` = :username and `password` = SHA2(CONCAT(:password, `salt`), 0)'
+            username = :username and passwordHash = SHA2(CONCAT(:passwordAttempt, salt), 0)'
             );
     
-        $query->bindValue(':password', $password);
+        $query->bindValue(':passwordAttempt', $password);
         $query->bindValue(':username', $username);
         
         $query->execute();
@@ -37,9 +40,6 @@ function checkPassword($username, $password) {
     } catch (PDOException $error) {
         echo "Failed: ", $error->getMessage();
         return false;
-    }
-    
-
-    
+    }    
 }
 ?>

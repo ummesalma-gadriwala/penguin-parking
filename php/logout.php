b3b892e57a6e6b35d5b2b80495418a6cb1a409e4
UmmeSalmaShabbir Gadriwala <gadriwau@mcmaster.ca>
2018-11-24 19:02:54 -0500

    sign in sign out conditional in menu

--- php/logout.php
+++ php/logout.php
@@ -1,10 +1,9 @@
 <?php
-    // session_start();
+    session_start();
     unset($_SESSION['isLoggedIn']);
     unset($_SESSION['username']);
     session_destroy();
     echo "Logout successful.";
     header("Location: http://" . $_SERVER['HTTP_HOST'] . "/index.php");
     exit();
-}
 ?>
--- php/menu.php
+++ php/menu.php
@@ -1,11 +1,21 @@
 <?php
 // Menu include file with HTML code for menu
-echo 
+session_start();
+if (isset($_SESSION['isLoggedIn'])) {
+    echo 
+    '<ul class="menu">
+        <li><a href="search.php">Search</a></li>
+        <li><a href="register.php">New User</a></li>
+        <li><a href="submission.php">New Parking Space</a></li>
+        <li><a href="logout.php">Sign out</a></li>  
+    </ul>';
+} else {
+    echo 
     '<ul class="menu">
         <li><a href="search.php">Search</a></li>
         <li><a href="register.php">New User</a></li>
         <li><a href="submission.php">New Parking Space</a></li>
         <li><a href="index.php">Sign in</a></li>
-        <li><a href="../logout.php">Sign out</a></li> 
-    </ul>'; //TODO: conditional signin signout display
+    </ul>';
+}
 ?>
